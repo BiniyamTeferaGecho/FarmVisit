c86c611cf65ab3070cbcd8951dd3b77af7ddeca1
Biniyam Tefera <biniyam-tefera@example.com>
2025-12-01 17:05:25 +0300

    chore: remove dist from repo and ignore it

--- dist/assets/user-DBa84ajX.js
+++
@@ -1,1 +0,0 @@
-import{G as e}from"./index-gIg6QK8U.js";const c=[["path",{d:"M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2",key:"975kel"}],["circle",{cx:"12",cy:"7",r:"4",key:"17ys0d"}]],r=e("user",c);export{r as U};
